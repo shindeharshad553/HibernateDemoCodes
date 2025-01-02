e61eef6e0584a360e7423d241d8a96292c2c2c32
shindeharshad553 <125859145+shindeharshad553@users.noreply.github.com>
2025-01-02 20:16:29 +0530

    Add files via upload

---
+++ com/mainclass/UpdateRecord.java
@@ -0,0 +1,29 @@
+package com.mainclass;
+
+import com.entity.Student;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class UpdateRecord {
+
+	public static void main(String[] args) {
+
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		cfg.addAnnotatedClass(Student.class);
+
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session s = sf.openSession();
+		Transaction tr = s.beginTransaction();
+
+		int id = 2;
+		Student stud = s.get(Student.class, id);
+		stud.setName("karan");
+		s.update(stud);
+		tr.commit();
+
+	}
+
+}
